84e4a5073da6f3328f7459449ae9f40a73442c36
Shubham-Soni-Coder <slsitu1424@gmail.com>
2025-04-05 15:02:48 +0530

    view_tasks() complete

---
+++ To_do_list/main.py
@@ -0,0 +1,54 @@
+import json
+def view_tasks():
+    try:
+        with open("tasks.json",'r') as file:
+            tasks = json.load(file)
+            if tasks:
+                print("Tasks:")
+                for task in tasks:
+                    print(f'[✓] {task["task"]}') if task["done"] else print(f'[ ] {task["task"]}')
+            else:   
+                print("No tasks found.")        
+    except FileNotFoundError:
+        print("No tasks found. Please add a task first.")               
+
+def add_tasks():
+    pass
+def edit_tasks():
+    pass
+def delete_tasks():
+    pass
+def save_tasks():
+    pass
+
+
+def start():
+    print("Welcome to the To-Do List App!")
+    while True:
+        print("1. View Tasks")
+        print("2. Add Task")
+        print("3. Edit Task")
+        print("4. Delete Task")
+        print("5. Save Tasks")
+        print("6. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            view_tasks()
+        elif choice == '2':
+            add_tasks()
+        elif choice == '3':
+            edit_tasks()
+        elif choice == '4':
+            delete_tasks()
+        elif choice == '5':
+            save_tasks()
+        elif choice == '6':
+            print("Exiting the app.")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+if __name__ == "__main__":
+    view_tasks()
